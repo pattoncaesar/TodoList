d4f0334fe6abb911950f402ae7cddf61a0b06340
changkaijie <changkaijie@core-tech.tw>
2020-07-14 10:07:36 +0800

    Login Success-> Tasks

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,13 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
+     * The path to the "tasks" route for your application.
+     *
+     * @var string
+     */
+    public const TASK = '/tasks';
+
+    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
